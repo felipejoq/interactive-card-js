e5d144d42b4defc9d55735f5cdbcfdda306def71
felipejoq <felipejofreq@gmail.com>
2023-08-01 12:39:26 -0400

    Tarjeta interactiva - primer commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## 🗂️ Experimental Card with JavaScript
+
+🚀 Este es un proyecto desarrollado con HTML + CSS + JavaScipt y que es parte del desafío
+que queda explicado en [este archivo pdf](https://github.com/felipejoq/interactive-card-js/blob/main/assets/media/challenge_explained.pdf?raw=true) y que se trata de confeccionar una tarjeta html
+que tenga interacción con la ayuda de JavaScript.
+
+✨ Al ingresar una cantidad y elegir un color, la tarjeta no hace nada, pero todo se gatilla
+con el evento "click" que se le añade al botón "Get Total", al presionar este botón se ejecutan
+varias funciones que por una parte calculan el total (cantidad por precio) y por otra modifican
+los estilos CSS, aplicando así manipulación del DOM con JavaScript.
+
+👉 [Live Demo Here](https://felipejoq.github.io/interactive-card-js.git) 👈
+
+### 📚 Stack utilizado:
+- Html
+- CSS
+- JS Vanilla
+
+### Vista previa:
+
+![Vista previa de la tarjeta interactiva](https://github.com/felipejoq/interactive-card-js/blob/main/assets/img/preview.jpg?raw=true)
+
---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/**
+ * Constants
+ **/
+const PRICE_SUCCULENT_POTS = 10000;
+
+/**
+ * Html elements
+ **/
+const quantityInput = document.getElementById('quantity')
+const color = document.getElementById('color')
+const btnGetTotal = document.getElementById('btnGetTotal')
+const result = document.getElementById('result')
+const total = document.getElementById('total');
+const amountFinal = document.getElementById('amountFinal');
+const colorSelected = document.getElementById('colorSelected');
+
+/**
+ * Actions and Events
+ **/
+btnGetTotal.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    total.innerText = calAmount(quantityInput.value, PRICE_SUCCULENT_POTS)
+    amountFinal.innerText = quantityInput.value
+    changeColorHtmlElement(colorSelected, color.value)
+})
+
+/**
+ * Globals functions and helpers
+ **/
+const mapColors = new Map()
+mapColors.set('#3dbbff', 'skyblue')
+mapColors.set('#469d37', 'green')
+mapColors.set('#c95b4f', 'terracotta')
+mapColors.set('#b02059', 'purple')
+
+const clpCurrency = new Intl.NumberFormat('es-CL', {
+    style: 'currency',
+    currency: 'CLP',
+});
+
+const calAmount = (amount, price) => {
+    return clpCurrency.format(amount * price);
+}
+
+const changeColorHtmlElement = (element, color) => {
+    result.style.display = 'flex';
+    element.innerText = mapColors.get(color) ?? 'Color';
+    element.style.background = color;
+}
+
